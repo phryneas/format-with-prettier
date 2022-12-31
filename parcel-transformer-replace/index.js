c11f19038726979916cf5168fc0a7830aae75aa8
Lenz Weber <mail@lenzw.de>
2022-12-31 19:12:11 +0100

    build adjustments

--- parcel-transformer-replace/index.js
+++ parcel-transformer-replace/index.js
@@ -1,10 +1,8 @@
 exports.default = new (require("@parcel/plugin").Transformer)({
   async transform({ asset }) {
-    if (asset.filePath.includes("prettier")) {
+    if (asset.filePath.includes("node_modules")) {
       // remove an eval for `globalThis` from some dependencies - those would trigger the DANGEROUS_EVAL web-ext lint rule
-      asset.setCode(
-        (await asset.getCode()).replace(`||Function("return this")()`, "")
-      );
+      asset.setCode((await asset.getCode()).replace(/Function\(['"]return this['"]\)\(\)/, "globalThis"));
     }
 
     return [asset];
